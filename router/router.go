1dd7bba74c39b26a31b0fd931199a1d0f25ae9b3
Gomes <gdemonst@gmail.com>
2024-01-23 16:30:15 -0300

    feat: error fix

---
+++ router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	// Initialize Router
+	router := gin.Default()
+
+	// Initialize Routes
+
+	// Initialize server
+	router.Run("8080")
+}
