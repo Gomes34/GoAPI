558bc5f6fb3ad92d82e9724ab9731fe6b692b1dc
Gomes <gdemonst@gmail.com>
2024-01-25 15:04:44 -0300

    feat: error validation

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -8,6 +8,11 @@ func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
 	ctx.BindJSON(&request)
 
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		return
+	}
+
 	if err := db.Create(&request).Error; err != nil {
 		logger.Errorf("error creaing opening: %v", err.Error())
 		return
--- handler/request.go
+++ handler/request.go
@@ -1,12 +1,40 @@
 package handler
 
+import "fmt"
+
 // CreateOpening
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
 
 type CreateOpeningRequest struct {
 	Role     string `json: "role"`
 	Company  string `json: "company"`
-	Locatoin string `json: "location"`
-	Remote   string `json: "remote"`
+	Location string `json: "location"`
+	Remote   *bool  `json: "remote"`
 	Link     string `json: "link"`
-	Salary   string `json: "salary"`
+	Salary   int64  `json: "salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errParamIsRequired("Comapny", "string")
+	}
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+	if r.Salary <= 0 {
+		return errParamIsRequired("salary", "int64")
+	}
+	return nil
 }
