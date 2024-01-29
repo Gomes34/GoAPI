1e368d38996217ad8d684b8bf188dd57130849e9
Gomes <gdemonst@gmail.com>
2024-01-29 18:45:27 -0300

    feat: add show opening

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,11 +3,22 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Gomes34/GoAPI/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-openng", opening)
+
 }
