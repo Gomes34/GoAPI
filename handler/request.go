5659c27f8570f0e1aaef5203aa5f98406473e591
Gomes <gdemonst@gmail.com>
2024-01-29 18:45:45 -0300

    feat: feat add update handler

--- handler/request.go
+++ handler/request.go
@@ -41,3 +41,21 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+// UpdateOpening
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid fiel must be provided")
+}
