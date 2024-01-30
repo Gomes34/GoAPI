90e1869e79eba2a684f54937d36eca785469521c
Gomes <gdemonst@gmail.com>
2024-01-30 13:36:17 -0300

    feat: swagger config

--- router/routes.go
+++ router/routes.go
@@ -9,22 +9,19 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
-	// Initialize handler
-	handler.InitalizeHandler()
+	// Initialize Handler
+	handler.InitializeHandler()
 	basePath := "/api/v1"
 	docs.SwaggerInfo.BasePath = basePath
-
 	v1 := router.Group(basePath)
 	{
-		// Show opening
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
-		v1.GET("/openings", handler.ListOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
+
 	}
 	// Initialize Swagger
-
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	router.Run(":8080")
 }
